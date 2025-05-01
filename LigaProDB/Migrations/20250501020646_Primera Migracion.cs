c55853ba75f0357b9a47e186fdfd39d95b8c5117
Sierra10 <daniel.sierra@udla.edu.ec>
2025-04-30 21:16:28 -0500

    Primer Commit -Se genero los controladores y se huzo los modelos, de igual manera se realizo la migracion a la base de datos

---
+++ LigaProDB/Migrations/20250501020646_Primera Migracion.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LigaProDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class PrimeraMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Equipo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Logo = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    PartidosJugados = table.Column<int>(type: "int", nullable: false),
+                    PartidosGanados = table.Column<int>(type: "int", nullable: false),
+                    PartidosEmpatados = table.Column<int>(type: "int", nullable: false),
+                    PartidosPerdidos = table.Column<int>(type: "int", nullable: false),
+                    Puntos = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jugador",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Edad = table.Column<int>(type: "int", nullable: true),
+                    EquipoId = table.Column<int>(type: "int", nullable: false),
+                    NumeroCamiseta = table.Column<int>(type: "int", nullable: false),
+                    Goles = table.Column<int>(type: "int", nullable: false),
+                    Asistencias = table.Column<int>(type: "int", nullable: false),
+                    Sueldo = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Posicion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jugador", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Jugador_Equipo_EquipoId",
+                        column: x => x.EquipoId,
+                        principalTable: "Equipo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jugador_EquipoId",
+                table: "Jugador",
+                column: "EquipoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jugador");
+
+            migrationBuilder.DropTable(
+                name: "Equipo");
+        }
+    }
+}
